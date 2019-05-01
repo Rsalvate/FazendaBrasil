5c5e14bc7cc84f187ec2e6ad9bd60458bfb8cf6a
Rodrigo Salvate Brasil <rodrigosalvate@hotmail.com>
2019-05-01 16:29:49 -0300

    Updating migrations.

---
+++ FazendaBrasil/FazendaBrasil/Migrations/20190501192842_AddingUniqueFrequencyName.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace FazendaBrasil.Migrations
+{
+    public partial class AddingUniqueFrequencyName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Frequencies",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Frequencies_Name",
+                table: "Frequencies",
+                column: "Name",
+                unique: true,
+                filter: "[Name] IS NOT NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Frequencies_Name",
+                table: "Frequencies");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Frequencies",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
